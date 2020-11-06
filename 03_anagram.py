4b6ec911a9ebf57a613166e7807a2478c097cfc6
Michael Shuff <mjshuff23@gmail.com>
2020-11-05 23:31:33 -0500

    finished count_and_compare

--- 03_anagram.py
+++ 03_anagram.py
@@ -27,23 +27,53 @@
 # # print(anagram_checking_off('abcd', 'dcba'))
 # print(anagram_checking_off('abcd', 'dcbc'))
 
-# 2. Sort and Compare
+# 2. Sort and Compare O(nlog(n))
 #     - use sorted to sort both and compare
 
-from itertools import zip_longest
+# from itertools import zip_longest
 
-def anagram_sort_and_compare2(s1, s2):
-    for a, b in zip_longest(sorted(s1), sorted(s2)):
+# def anagram_sort_and_compare2(s1, s2):
+#     for a, b in zip_longest(sorted(s1), sorted(s2)):
+#         if a != b:
+#             return False
+#     return True
+
+# def anagram_sort_and_compare(str1, str2):
+#     for a, b in zip_longest(sorted(str1), sorted(str2)):
+#         if a != b:
+#             return False
+#     return True
+
+
+# print(anagram_sort_and_compare('abcde', 'edcba'))  # => True
+# print(anagram_sort_and_compare('abcde', 'abcd'))   # => False
+
+# 3. Brute Force -- Don't do.  Factorial growth
+# 4. Count and Compare
+#    - Anagrams will always have the same number of any character they share
+#   A. Generate character count from each string
+#   B. If these counts match the two string are anagrams
+
+def anagram_count_compare(s1, s2):
+    c1 = [0] * 26  # creates a list with 26 slots
+    c2 = [0] * 26
+
+    for char in s1:
+        # The ord() function returns an integer representing the Unicode character.
+        # Here, we are finding taking the unicode of the char and subtracting the unicode
+        # of the beginning of the lowercase alphabet to get it's character spot as 1-26
+        pos = ord(char) - ord('a')
+        c1[pos] += 1
+
+    for char in s2:
+        pos = ord(char) - ord('a')
+        c2[pos] += 1
+    # for each index of both strings...
+    for a, b in zip(c1, c2):
         if a != b:
             return False
     return True
 
-def anagram_sort_and_compare(str1, str2):
-    for a, b in zip_longest(sorted(str1), sorted(str2)):
-        if a != b:
-            return False
-    return True
 
-
-print(anagram_sort_and_compare('abcde', 'edcba'))  # => True
-print(anagram_sort_and_compare('abcde', 'abcd'))   # => False
+print(anagram_count_compare('apple', 'pleap'))  # => True
+print(anagram_count_compare('apple', 'applf'))
