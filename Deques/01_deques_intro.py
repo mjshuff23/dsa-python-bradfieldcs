ed10f90f907dce81884914db63834193b3036010
Michael Shuff <mjshuff23@gmail.com>
2020-11-07 16:37:41 -0500

    added deques folder and intro

---
+++ Deques/01_deques_intro.py
@@ -0,0 +1,7 @@
+# A deque(double-ended queue), is an ordered collection of items similar to the queue.
+#  - It has two ends, a front and a rear, and the items remain positioned in the collection.
+#  - What makes a deque different is the unrestrictive nature of adding and removing items.
+#      - New items can be added at either the front or the rear.
+#      - Existing items can be removed from either end.
+#  - This hybrid linear structure provides all the capabilities of stacks and queues in a
+#    single data structure.
